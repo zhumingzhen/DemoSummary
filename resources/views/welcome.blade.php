354ab4a81fa5778ac5688a3f36e2974282c5b68b
zhumingzhen <itdream53@gmail.com>
2018-01-05 15:48:17 +0800

    hebing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 111
+                    Laravel 111222
                 </div>
 
                 <div class="links">
