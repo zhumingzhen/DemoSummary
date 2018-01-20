0b0794e212de2bde1b11bdf015a0aa95d4e36f2a
zhumingzhen <itdream53@gmail.com>
2018-01-20 23:32:04 +0800

    bug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 111222
+                    Laravel 111222666
                 </div>
 
                 <div class="links">
